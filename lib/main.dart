79f4b7fafd677211f7e7853c5503ea70d119615f
Haris Samingan <muhamad_haris@hotmail.com>
2021-04-26 16:05:10 +0800

    Add sign up account page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/material.dart';
 
 final signupPurpleGradient = LinearGradient(colors: [
@@ -31,7 +32,168 @@ class MyApp extends StatelessWidget {
 class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final topPadding = MediaQuery.of(context).padding.top;
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromRGBO(145, 131, 222, 1),
+          Color.fromRGBO(160, 148, 227, 1),
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: DefaultTextStyle(
+          style: TextStyle(
+            fontFamily: 'SFPro Display',
+            color: Colors.white,
+          ),
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: [
+                SizedBox(height: topPadding),
+                SizedBox(height: 10),
+                // SignUpImage(),
+                AnimatedImage(),
+                SizedBox(height: 5),
+                Text(
+                  'Hi there!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white.withOpacity(.75),
+                  ),
+                ),
+                SizedBox(height: 6),
+                Text(
+                  "Let's Get Started",
+                  style: TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                SizedBox(height: 37),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 36),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 28, horizontal: 22),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(37),
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.zero,
+                            isDense: true,
+                            border: InputBorder.none,
+                            hintText: 'Username',
+                            hintStyle: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            icon: Image.asset('assets/icons/user.png'),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 14),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 28, horizontal: 22),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(37),
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.zero,
+                            isDense: true,
+                            border: InputBorder.none,
+                            hintText: 'Password',
+                            hintStyle: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            icon: Image.asset('assets/icons/key.png'),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 14),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Create an Account',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            shadowColor: Color.fromRGBO(82, 67, 154, 0.25),
+                            elevation: 0,
+                            padding: EdgeInsets.symmetric(vertical: 26),
+                            primary: Color.fromRGBO(82, 67, 154, 1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(37),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 32),
+                      Row(
+                        children: [
+                          Expanded(child: DottedLine()),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'Or',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          Expanded(child: DottedLine()),
+                        ],
+                      ),
+                      SizedBox(height: 32),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Log In',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            shadowColor: Color.fromRGBO(82, 67, 154, 0.25),
+                            elevation: 0,
+                            padding: EdgeInsets.symmetric(vertical: 26),
+                            primary: Color.fromRGBO(255, 255, 255, 0.28),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(37),
+                            ),
+                          ).copyWith(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -39,5 +201,43 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+}
+
+class AnimatedImage extends StatefulWidget {
+  @override
+  _AnimatedImageState createState() => _AnimatedImageState();
+}
+
+class _AnimatedImageState extends State<AnimatedImage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 3),
+  )..repeat(reverse: true);
+
+  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
+    begin: Offset.zero,
+    end: Offset(0, 0.08),
+  ).animate(_controller);
+  // .animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset('assets/images/clouds.png'),
+        SlideTransition(
+          position: _offsetAnimation,
+          child: Image.asset('assets/images/person_on_rocket.png'),
+        ),
+      ],
+    );
   }
 }
