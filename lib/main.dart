f455d25e6556c2b1877a2d9520e29c372f11c595
Haris Samingan <muhamad_haris@hotmail.com>
2021-05-03 17:13:54 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,64 @@ class MyApp extends StatelessWidget {
 class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final topPadding = MediaQuery.of(context).padding.top;
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromRGBO(145, 131, 222, 1),
+          Color.fromRGBO(160, 148, 227, 1),
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(height: topPadding),
+              SizedBox(height: 10),
+              AnimatedImage(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
-class LoginScreen extends StatelessWidget {
+class AnimatedImage extends StatefulWidget {
+  @override
+  _AnimatedImageState createState() => _AnimatedImageState();
+}
+
+class _AnimatedImageState extends State<AnimatedImage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 3),
+  )..repeat(reverse: true);
+
+  late final Animation<Offset> _animation = Tween<Offset>(
+    begin: Offset.zero,
+    end: Offset(0, 0.08),
+  ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Image.asset('assets/images/clouds.png'),
+        SlideTransition(
+          position: _animation,
+          child: Image.asset('assets/images/rocket_person.png'),
+        ),
+      ],
+    );
   }
 }
